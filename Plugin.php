ba88eec162c4e8420f6bacb4e0be13c2a0de279d
AlanDecode <zytbuaa1415@gmail.com>
2018-06-03 23:50:01 +0800

    微调样式

--- Plugin.php
+++ Plugin.php
@@ -7,11 +7,11 @@
  * 
  * @package PandaBangumi
  * @author 熊猫小A
- * @version 0.99.4
+ * @version 0.99.5
  * @link https://imalan.cn
  */
 
-define('PandaBangumi_Plugin_VERSION', '0.99.4');
+define('PandaBangumi_Plugin_VERSION', '0.99.5');
 
 class PandaBangumi_Plugin implements Typecho_Plugin_Interface
 {
