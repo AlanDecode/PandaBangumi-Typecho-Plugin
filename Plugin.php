14e3b215dce0c7e6b54eee8d2b9774c9e9febe6e
AlanDecode <zytbuaa1415@gmail.com>
2018-08-28 20:55:54 +0800

    update

--- Plugin.php
+++ Plugin.php
@@ -7,11 +7,11 @@
  * 
  * @package PandaBangumi
  * @author 熊猫小A
- * @version 1.21
+ * @version 1.3
  * @link https://imalan.cn
  */
 
-define('PandaBangumi_Plugin_VERSION', '1.21');
+define('PandaBangumi_Plugin_VERSION', '1.3');
 
 class PandaBangumi_Plugin implements Typecho_Plugin_Interface
 {
